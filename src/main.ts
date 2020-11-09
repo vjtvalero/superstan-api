f38978542d6c308e0d0bc7980c0b6c54e781ca92
Vincent James <viayaqtx>
2020-11-09 22:57:35 +0800

    port

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: { origin: [process.env.FRONTEND_URL || 'http://localhost:4000'] } });
-    const config = app.get(ConfigService);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-    await app.listen(config.get<number>('port'));
+    await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
